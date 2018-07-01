ce36d93d889db29ecc33e9585e0a65134d035291
Sacharomyces <b.mackiewicz1@gmail.com>
2018-07-01 21:41:51 +0200

    nodejs repo added

---
+++ Learn and undearstand nodejs/Modules/greet.js
@@ -0,0 +1,5 @@
+var greet = function () {
+    console.log("yo");
+};
+
+module.exports = greet; 
---
+++ Learn and undearstand nodejs/app.js
@@ -0,0 +1,6 @@
+// a function to pass location of a module to import and execute it.
+//if module exports fome functions it will be returned from require function.
+var greet = require("./Modules/greet");
+
+greet();
+//
